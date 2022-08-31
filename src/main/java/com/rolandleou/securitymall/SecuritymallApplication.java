8b0d32a80cd7536a5526ab553b175ed72862ece9
Roland Liu <rolandleou@gmail.com>
2022-08-31 19:28:19 +0800

    Initial E-commerence spring boot project

---
+++ src/main/java/com/rolandleou/securitymall/SecuritymallApplication.java
@@ -0,0 +1,13 @@
+package com.rolandleou.securitymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritymallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritymallApplication.class, args);
+	}
+
+}
